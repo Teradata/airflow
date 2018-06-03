Teradata/airflow

commit 7153d557ef5edeb2820927dab47ced1c4c745a03

[AIRFLOW-2525] Fix PostgresHook.copy_expert to work with "COPY FROM"
For now PostgresHook.copy_expert supports
"COPY TO" but not "COPY FROM", because it
opens a file with write mode and doesn't
commit operations. This PR fixes it by
opening a file with read and write mode
and committing operations at last.

airflow/hooks/postgres_hook.py
@@ -64,10 +64,11 @@ class PostgresHook(DbApiHook):
         Executes SQL using psycopg2 copy_expert method
         Necessary to execute COPY command without access to a superuser
         """
-        f = open(filename, 'w')
-        with closing(self.get_conn()) as conn:
-            with closing(conn.cursor()) as cur:
-                cur.copy_expert(sql, f)
+        with open(filename, 'w+') as f:
+            with closing(self.get_conn()) as conn:
+                with closing(conn.cursor()) as cur:
+                    cur.copy_expert(sql, f)
+                    conn.commit()
 
     @staticmethod
     def _serialize_cell(cell, conn):

tests/hooks/test_postgres_hook.py
@@ -43,17 +43,16 @@ class TestPostgresHook(unittest.TestCase):
 
     def test_copy_expert(self):
         m = mock.mock_open(read_data='{"some": "json"}')
-        with mock.patch('airflow.hooks.postgres_hook.open', m, create=True) as m:
+        with mock.patch('airflow.hooks.postgres_hook.open', m):
             statement = "SQL"
             filename = "filename"
 
             self.cur.fetchall.return_value = None
-            f = m(filename, 'w')
-            def test_open(filename, mode):
-                return f
 
-            self.assertEqual(None, self.db_hook.copy_expert(statement, filename, open=test_open))
+            self.assertEqual(None, self.db_hook.copy_expert(statement, filename, open=m))
 
             self.conn.close.assert_called_once()
             self.cur.close.assert_called_once()
-            self.cur.copy_expert.assert_called_once_with(statement, f)
+            self.conn.commit.assert_called_once()
+            self.cur.copy_expert.assert_called_once_with(statement, m.return_value)
+            m.assert_called_once_with(filename, "w+")

commit 865666e2bec78a5f892da66b5b320bc2f9a93615

[AIRFLOW-2525] Fix a bug introduced by commit 7153d55
The previous commit on this issue (#3421)
introduced a new bug on COPY FROM operation.
This PR fixes it by opening a file with 'r+'
mode instead of 'w+' to avoid truncating it.

Closes #3423 from sekikn/AIRFLOW-2525-2

airflow/hooks/postgres_hook.py
@@ -17,6 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
 import psycopg2
 import psycopg2.extensions
 from contextlib import closing
@@ -61,13 +62,24 @@ class PostgresHook(DbApiHook):
 
     def copy_expert(self, sql, filename, open=open):
         """
-        Executes SQL using psycopg2 copy_expert method
-        Necessary to execute COPY command without access to a superuser
+        Executes SQL using psycopg2 copy_expert method.
+        Necessary to execute COPY command without access to a superuser.
+
+        Note: if this method is called with a "COPY FROM" statement and
+        the specified input file does not exist, it creates an empty
+        file and no data is loaded, but the operation succeeds.
+        So if users want to be aware when the input file does not exist,
+        they have to check its existence by themselves.
         """
-        with open(filename, 'w+') as f:
+        if not os.path.isfile(filename):
+            with open(filename, 'w'):
+                pass
+
+        with open(filename, 'r+') as f:
             with closing(self.get_conn()) as conn:
                 with closing(conn.cursor()) as cur:
                     cur.copy_expert(sql, f)
+                    f.truncate(f.tell())
                     conn.commit()
 
     @staticmethod

tests/hooks/test_postgres_hook.py
@@ -55,4 +55,4 @@ class TestPostgresHook(unittest.TestCase):
             self.cur.close.assert_called_once()
             self.conn.commit.assert_called_once()
             self.cur.copy_expert.assert_called_once_with(statement, m.return_value)
-            m.assert_called_once_with(filename, "w+")
+            self.assertEqual(m.call_args[0], (filename, "r+"))