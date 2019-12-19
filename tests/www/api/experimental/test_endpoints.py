apache/spark

[AIRFLOW-3632] Only replace microseconds if execution_date is None in trigger_dag REST API (#6380)
No need to require a user to pass in replace_microseconds to the
request body; instead we should use exactly the date that is given.
We will still replace the microseconds on execution_date if none is
passed in (and the param is True, which is the default)

tests/www/api/experimental/test_endpoints.py
@@ -129,13 +129,17 @@ class TestApiExperimental(TestBase):
         )
 
         self.assertEqual(200, response.status_code)
+        response_execution_date = parse_datetime(json.loads(response.data.decode('utf-8'))['execution_date'])
+        self.assertEqual(0, response_execution_date.microsecond)
+
         # Check execution_date is correct
         response = json.loads(response.data.decode('utf-8'))
         dagbag = DagBag()
         dag = dagbag.get_dag('example_bash_operator')
-        dag_run = dag.get_dagrun(parse_datetime(response['execution_date']))
+        dag_run = dag.get_dagrun(response_execution_date)
         self.assertEqual(run_id, dag_run.run_id)
 
+        # Test error for nonexistent dag
         response = self.client.post(
             url_template.format('does_not_exist_dag'),
             data=json.dumps({}),
@@ -146,14 +150,10 @@ class TestApiExperimental(TestBase):
     def test_trigger_dag_for_date(self):
         url_template = '/api/experimental/dags/{}/dag_runs'
         dag_id = 'example_bash_operator'
-        hour_from_now = utcnow() + timedelta(hours=1)
-        execution_date = datetime(hour_from_now.year,
-                                  hour_from_now.month,
-                                  hour_from_now.day,
-                                  hour_from_now.hour)
+        execution_date = utcnow() + timedelta(hours=1)
         datetime_string = execution_date.isoformat()
 
-        # Test Correct execution
+        # Test correct execution with execution date
         response = self.client.post(
             url_template.format(dag_id),
             data=json.dumps({'execution_date': datetime_string}),
@@ -169,10 +169,27 @@ class TestApiExperimental(TestBase):
                         'Dag Run not found for execution date {}'
                         .format(execution_date))
 
+        # Test correct execution with execution date and microseconds replaced
+        response = self.client.post(
+            url_template.format(dag_id),
+            data=json.dumps({'execution_date': datetime_string, 'replace_microseconds': 'true'}),
+            content_type="application/json"
+        )
+        self.assertEqual(200, response.status_code)
+        response_execution_date = parse_datetime(json.loads(response.data.decode('utf-8'))['execution_date'])
+        self.assertEqual(0, response_execution_date.microsecond)
+
+        dagbag = DagBag()
+        dag = dagbag.get_dag(dag_id)
+        dag_run = dag.get_dagrun(response_execution_date)
+        self.assertTrue(dag_run,
+                        'Dag Run not found for execution date {}'
+                        .format(execution_date))
+
         # Test error for nonexistent dag
         response = self.client.post(
             url_template.format('does_not_exist_dag'),
-            data=json.dumps({'execution_date': execution_date.isoformat()}),
+            data=json.dumps({'execution_date': datetime_string}),
             content_type="application/json"
         )
         self.assertEqual(404, response.status_code)

Teradata/airflow

(cherry picked from commit 9e6e4d804d76875697f1c1c20d3f8632d851c2ff)

tests/www/api/experimental/test_endpoints.py
@@ -121,13 +121,17 @@ class TestApiExperimental(unittest.TestCase):
         )
 
         self.assertEqual(200, response.status_code)
+        response_execution_date = parse_datetime(json.loads(response.data.decode('utf-8'))['execution_date'])
+        self.assertEqual(0, response_execution_date.microsecond)
+
         # Check execution_date is correct
         response = json.loads(response.data.decode('utf-8'))
         dagbag = DagBag()
         dag = dagbag.get_dag('example_bash_operator')
-        dag_run = dag.get_dagrun(parse_datetime(response['execution_date']))
+        dag_run = dag.get_dagrun(response_execution_date)
         self.assertEqual(run_id, dag_run.run_id)
 
+        # Test error for nonexistent dag
         response = self.app.post(
             url_template.format('does_not_exist_dag'),
             data=json.dumps({}),
@@ -158,14 +162,10 @@ class TestApiExperimental(unittest.TestCase):
     def test_trigger_dag_for_date(self):
         url_template = '/api/experimental/dags/{}/dag_runs'
         dag_id = 'example_bash_operator'
-        hour_from_now = utcnow() + timedelta(hours=1)
-        execution_date = datetime(hour_from_now.year,
-                                  hour_from_now.month,
-                                  hour_from_now.day,
-                                  hour_from_now.hour)
+        execution_date = utcnow() + timedelta(hours=1)
         datetime_string = execution_date.isoformat()
 
-        # Test Correct execution
+        # Test correct execution with execution date
         response = self.app.post(
             url_template.format(dag_id),
             data=json.dumps({'execution_date': datetime_string}),
@@ -181,10 +181,27 @@ class TestApiExperimental(unittest.TestCase):
                         'Dag Run not found for execution date {}'
                         .format(execution_date))
 
+        # Test correct execution with execution date and microseconds replaced
+        response = self.app.post(
+            url_template.format(dag_id),
+            data=json.dumps({'execution_date': datetime_string, 'replace_microseconds': 'true'}),
+            content_type="application/json"
+        )
+        self.assertEqual(200, response.status_code)
+        response_execution_date = parse_datetime(json.loads(response.data.decode('utf-8'))['execution_date'])
+        self.assertEqual(0, response_execution_date.microsecond)
+
+        dagbag = DagBag()
+        dag = dagbag.get_dag(dag_id)
+        dag_run = dag.get_dagrun(response_execution_date)
+        self.assertTrue(dag_run,
+                        'Dag Run not found for execution date {}'
+                        .format(execution_date))
+
         # Test error for nonexistent dag
         response = self.app.post(
             url_template.format('does_not_exist_dag'),
-            data=json.dumps({'execution_date': execution_date.isoformat()}),
+            data=json.dumps({'execution_date': datetime_string}),
             content_type="application/json"
         )
         self.assertEqual(404, response.status_code)