apache/spark

[AIRFLOW-5072] gcs_hook should download once (#5685)
When a user supplied a filename the expected behaviour is that airflow
downloads the file and does not return it's content as a string.

airflow/contrib/operators/gcs_download_operator.py
@@ -34,6 +34,10 @@ class GoogleCloudStorageDownloadOperator(BaseOperator):
     """
     Downloads a file from Google Cloud Storage.
 
+    If a filename is supplied, it writes the file to the specified location, alternatively one can
+    set the ``store_to_xcom_key`` parameter to True push the file content into xcom. When the file size
+    exceeds the maximum size for xcom it is recommended to write to a file.
+
     :param bucket: The Google cloud storage bucket where the object is. (templated)
     :type bucket: str
     :param object: The name of the object to download in the Google cloud
@@ -81,6 +85,9 @@ class GoogleCloudStorageDownloadOperator(BaseOperator):
             else:
                 TypeError("__init__() missing 1 required positional argument: 'object_name'")
 
+        if filename is not None and store_to_xcom_key is not None:
+            raise ValueError("Either filename or store_to_xcom_key can be set")
+
         if google_cloud_storage_conn_id:
             warnings.warn(
                 "The google_cloud_storage_conn_id parameter has been deprecated. You should pass "
@@ -102,13 +109,17 @@ class GoogleCloudStorageDownloadOperator(BaseOperator):
             google_cloud_storage_conn_id=self.gcp_conn_id,
             delegate_to=self.delegate_to
         )
-        file_bytes = hook.download(bucket_name=self.bucket,
-                                   object_name=self.object,
-                                   filename=self.filename)
+
         if self.store_to_xcom_key:
+            file_bytes = hook.download(bucket_name=self.bucket,
+                                       object_name=self.object)
             if sys.getsizeof(file_bytes) < MAX_XCOM_SIZE:
                 context['ti'].xcom_push(key=self.store_to_xcom_key, value=file_bytes)
             else:
                 raise AirflowException(
                     'The size of the downloaded file is too large to push to XCom!'
                 )
+        else:
+            hook.download(bucket_name=self.bucket,
+                          object_name=self.object,
+                          filename=self.filename)

Teradata/airflow

(cherry-picked from d288460f61c04c9e4e2ea5f89355adc2b5d83450)

airflow/contrib/operators/gcs_download_operator.py
@@ -70,6 +70,10 @@ class GoogleCloudStorageDownloadOperator(BaseOperator):
                  *args,
                  **kwargs):
         super(GoogleCloudStorageDownloadOperator, self).__init__(*args, **kwargs)
+
+        if filename is not None and store_to_xcom_key is not None:
+            raise ValueError("Either filename or store_to_xcom_key can be set")
+
         self.bucket = bucket
         self.object = object
         self.filename = filename
@@ -84,13 +88,17 @@ class GoogleCloudStorageDownloadOperator(BaseOperator):
             google_cloud_storage_conn_id=self.google_cloud_storage_conn_id,
             delegate_to=self.delegate_to
         )
-        file_bytes = hook.download(bucket=self.bucket,
-                                   object=self.object,
-                                   filename=self.filename)
+
         if self.store_to_xcom_key:
+            file_bytes = hook.download(bucket=self.bucket,
+                                       object=self.object)
             if sys.getsizeof(file_bytes) < MAX_XCOM_SIZE:
                 context['ti'].xcom_push(key=self.store_to_xcom_key, value=file_bytes)
             else:
                 raise RuntimeError(
                     'The size of the downloaded file is too large to push to XCom!'
                 )
+        else:
+            hook.download(bucket=self.bucket,
+                          object=self.object,
+                          filename=self.filename)