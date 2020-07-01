apache/spark

Kubernetes Cluster is started on host not in the container (#8265)
Tests requiring Kubernetes Cluster are now moved out of
the regular CI tests and moved to "kubernetes_tests" folder
so that they can be run entirely on host without having
the CI image built at all. They use production image
to run the tests on KinD cluster and we add tooling
to start/stop/deploy the application to the KinD cluster
automatically - for both CI testing and local development.

This is a pre-requisite to convert the tests to convert the
tests to use the official Helm Chart and Docker images or
Apache Airflow.

It closes #8782

scripts/ci/kubernetes/docker/bootstrap.sh
@@ -1,0 +1,74 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+set -euo pipefail
+
+# This part is to allow fast iteration with the kubernetes tests
+# Pre-installed Airflow from the production image is removed and Airflow is re-installed from the
+# sources added during preparing the kubernetes image. This way when we deploy the image
+# to KinD we only add latest sources and only that most recent layer is sent to kind
+# and airflow always runs with compiled dist web files from pre-compiled dist installed in prod image
+
+
+echo
+echo "Save minimised web files"
+echo
+
+mv "$(python -m site | grep ^USER_SITE | awk '{print $2}' | tr -d "'")/airflow/www/static/dist/" \
+    "/tmp"
+
+echo
+echo "Uninstalling pre-installed airflow"
+echo
+
+# Uninstall preinstalled Apache Airlfow
+pip uninstall -y apache-airflow
+
+
+echo
+echo "Installing airflow from the sources"
+echo
+
+# Installing airflow from the sources copied to the Kubernetes image
+pip install --user "${AIRFLOW_SOURCES}"
+
+echo
+echo "Restore minimised web files"
+echo
+
+mv "/tmp/dist" "$(python -m site | grep ^USER_SITE | awk '{print $2}' | tr -d "'")/airflow/www/static/"
+
+echo
+echo "Airflow prepared. Running ${1}"
+echo
+
+
+if [[ "$1" = "webserver" ]]
+then
+    exec airflow webserver
+fi
+
+if [[ "$1" = "scheduler" ]]
+then
+    exec airflow scheduler
+fi
+
+echo
+echo "Entering bash"
+echo
+
+exec /bin/bash

Teradata/airflow

(cherry picked from commit a4520cd3751dec767793d2ac507d3f8a23454857)

scripts/ci/kubernetes/docker/bootstrap.sh
@@ -1,0 +1,74 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+set -euo pipefail
+
+# This part is to allow fast iteration with the kubernetes tests
+# Pre-installed Airflow from the production image is removed and Airflow is re-installed from the
+# sources added during preparing the kubernetes image. This way when we deploy the image
+# to KinD we only add latest sources and only that most recent layer is sent to kind
+# and airflow always runs with compiled dist web files from pre-compiled dist installed in prod image
+
+
+echo
+echo "Save minimised web files"
+echo
+
+mv "$(python -m site | grep ^USER_SITE | awk '{print $2}' | tr -d "'")/airflow/www_rbac/static/dist/" \
+    "/tmp"
+
+echo
+echo "Uninstalling pre-installed airflow"
+echo
+
+# Uninstall preinstalled Apache Airlfow
+pip uninstall -y apache-airflow
+
+
+echo
+echo "Installing airflow from the sources"
+echo
+
+# Installing airflow from the sources copied to the Kubernetes image
+pip install --user "${AIRFLOW_SOURCES}"
+
+echo
+echo "Restore minimised web files"
+echo
+
+mv "/tmp/dist" "$(python -m site | grep ^USER_SITE | awk '{print $2}' | tr -d "'")/airflow/www/static/"
+
+echo
+echo "Airflow prepared. Running ${1}"
+echo
+
+
+if [[ "$1" = "webserver" ]]
+then
+    exec airflow webserver
+fi
+
+if [[ "$1" = "scheduler" ]]
+then
+    exec airflow scheduler
+fi
+
+echo
+echo "Entering bash"
+echo
+
+exec /bin/bash