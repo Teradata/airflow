859304d7111388c6a150f6961184363dc89b4eda
Satish Ch <satishchinthanippu@gmail.com>
2023-11-21 00:44:11 -0800

    system tests howtoguide is added in detailed for transfer to transfer operator

--- tests/system/providers/teradata/example_teradata.py
+++ tests/system/providers/teradata/example_teradata.py
@@ -21,6 +21,7 @@ Example use of Teradata related operators.
 from __future__ import annotations
 
 import datetime
+import os
 
 import pytest
 
@@ -31,6 +32,14 @@ try:
 except ImportError:
     pytest.skip("TERADATA provider not available", allow_module_level=True)
 
+# [START teradata_operator_howto_guide]
+
+
+# create_table_teradata, insert_teradata_task, create_table_teradata_from_external_file, populate_user_table
+# get_all_countries, get_all_description, get_countries_from_continent, drop_table_teradata_task, drop_users_table_teradata_task
+# are examples of tasks created by instantiating the Teradata Operator
+
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
 DAG_ID = "example_teradata"
 
 with DAG(
@@ -40,8 +49,8 @@ with DAG(
     catchup=False,
     default_args={"conn_id": "teradata_default"},
 ) as dag:
-    # Example of creating a task to create a table in Teradata
-    create_table_teradata_task = TeradataOperator(
+    # [START teradata_operator_howto_guide_create_country_table]
+    create_country_table = TeradataOperator(
         task_id="create_country_table",
         sql=r"""
         CREATE TABLE Country (
@@ -51,12 +60,10 @@ with DAG(
         );
         """,
     )
-
-    # [END howto_operator_teradata]
-
-    # [START teradata_operator_howto_guide_insert_country_table]
-    insert_teradata_task = TeradataOperator(
-        task_id="insert_teradata_task",
+    # [END teradata_operator_howto_guide_create_country_table]
+    # [START teradata_operator_howto_guide_populate_country_table]
+    populate_country_table = TeradataOperator(
+        task_id="populate_country_table",
         sql=r"""
                 INSERT INTO Country VALUES ( 1, 'India', 'Asia');
                 INSERT INTO Country VALUES ( 2, 'Germany', 'Europe');
@@ -65,48 +72,19 @@ with DAG(
                 """,
     )
     # [END teradata_operator_howto_guide_populate_country_table]
-
-    # [START howto_operator_teradata]
-
-    # [START teradata_operator_howto_guide_create_table_teradata_from_external_file]
-    # Example of creating a task that calls an sql command from an external file.
-    create_table_teradata_from_external_file = TeradataOperator(
-        task_id="create_table_from_external_file",
+    # [START teradata_operator_howto_guide_create_users_table_from_external_file]
+    create_users_table_from_external_file = TeradataOperator(
+        task_id="create_users_table_from_external_file",
         sql="create_table.sql",
         dag=dag,
     )
-    # [END teradata_operator_howto_guide_create_table_teradata_from_external_file]
-
-    # [START teradata_operator_howto_guide_populate_user_table]
-    populate_user_table = TeradataOperator(
-        task_id="populate_user_table",
-        sql=r"""
-                INSERT INTO Users (username, description)
-                VALUES ( 'Danny', 'Musician');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Simone', 'Chef');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Lily', 'Florist');
-                INSERT INTO Users (username, description)
-                VALUES ( 'Tim', 'Pet shop owner');
-                """,
-    )
-    # [END teradata_operator_howto_guide_populate_user_table]
-
+    # [END teradata_operator_howto_guide_create_users_table_from_external_file]
     # [START teradata_operator_howto_guide_get_all_countries]
     get_all_countries = TeradataOperator(
         task_id="get_all_countries",
         sql=r"""SELECT * FROM Country;""",
     )
     # [END teradata_operator_howto_guide_get_all_countries]
-
-    # [START teradata_operator_howto_guide_get_all_description]
-    get_all_description = TeradataOperator(
-        task_id="get_all_description",
-        sql=r"""SELECT description FROM Users;""",
-    )
-    # [END teradata_operator_howto_guide_get_all_description]
-
     # [START teradata_operator_howto_guide_params_passing_get_query]
     get_countries_from_continent = TeradataOperator(
         task_id="get_countries_from_continent",
@@ -114,28 +92,26 @@ with DAG(
         params={"column": "continent", "value": "Asia"},
     )
     # [END teradata_operator_howto_guide_params_passing_get_query]
-
-    drop_table_teradata_task = TeradataOperator(
-        task_id="drop_table_teradata", sql=r"""DROP TABLE Country;""", dag=dag
+    # [START teradata_operator_howto_guide_drop_country_table]
+    drop_country_table = TeradataOperator(
+        task_id="drop_country_table", sql=r"""DROP TABLE Country;""", dag=dag
     )
-
-    drop_users_table_teradata_task = TeradataOperator(
-        task_id="drop_users_table_teradata", sql=r"""DROP TABLE Users;""", dag=dag
-    )
+    # [END teradata_operator_howto_guide_drop_country_table]
+    # [START teradata_operator_howto_guide_drop_users_table]
+    drop_users_table = TeradataOperator(task_id="drop_users_table", sql=r"""DROP TABLE Users;""", dag=dag)
+    # [END teradata_operator_howto_guide_drop_users_table]
 
     (
-        create_table_teradata_task
-        >> insert_teradata_task
-        >> create_table_teradata_from_external_file
-        >> populate_user_table
+        create_country_table
+        >> populate_country_table
+        >> create_users_table_from_external_file
         >> get_all_countries
-        >> get_all_description
         >> get_countries_from_continent
-        >> drop_table_teradata_task
-        >> drop_users_table_teradata_task
+        >> drop_country_table
+        >> drop_users_table
     )
 
-    # [END howto_operator_teradata]
+    # [END teradata_operator_howto_guide]
 
     from tests.system.utils.watcher import watcher
 
