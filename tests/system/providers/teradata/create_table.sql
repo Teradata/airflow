88f66966cdfaa27e9790d3b0d80bb6112811fe8a
Satish Ch <satishchinthanippu@gmail.com>
2024-01-03 02:30:25 +0530

    Modified table from SET to system default

--- tests/system/providers/teradata/create_table.sql
+++ tests/system/providers/teradata/create_table.sql
@@ -16,7 +16,7 @@
  specific language governing permissions and limitations
  under the License.
 */
-CREATE SET TABLE Users, FALLBACK (
+CREATE TABLE Users, FALLBACK (
         username   varchar(50),
         description           varchar(256)
     );
