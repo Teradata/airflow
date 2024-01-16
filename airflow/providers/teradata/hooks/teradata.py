2b8f087ce3aba49ce98b6e44fe91139160e37f8e
Satish Ch <satishchinthanippu@gmail.com>
2024-01-16 19:25:26 +0530

    Update teradata.py

--- airflow/providers/teradata/hooks/teradata.py
+++ airflow/providers/teradata/hooks/teradata.py
@@ -162,12 +162,12 @@ class TeradataHook(DbApiHook):
             "password": conn.password or "dbc",
         }
 
-        self.log.info("satish: selected tmode is %s", conn.extra_dejson["tmode"])
         if conn.extra_dejson.get("tmode", False):
+            self.log.info("satish: selected tmode is %s", conn.extra_dejson["tmode"])
             conn_config["tmode"] = conn.extra_dejson["tmode"]
 
         # Handling SSL connection parameters
-
+        self.log.info("satish: selected sslmode is %s", conn.extra_dejson["sslmode"])
         if conn.extra_dejson.get("sslmode", False):
             conn_config["sslmode"] = conn.extra_dejson["sslmode"]
             if "verify" in conn_config["sslmode"]:
