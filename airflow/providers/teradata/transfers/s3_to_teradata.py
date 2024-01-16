75162e0f542078b2e794c1608674fdd9d3045b7d
Satish Ch <satishchinthanippu@gmail.com>
2024-01-16 09:38:46 +0530

    Update s3_to_teradata.py

--- airflow/providers/teradata/transfers/s3_to_teradata.py
+++ airflow/providers/teradata/transfers/s3_to_teradata.py
@@ -34,7 +34,7 @@ class S3ToTeradataOperator(BaseOperator):
     Moves data from Teradata source database to Teradata destination database.
     .. seealso::
     For more information on how to use this operator, take a look at the guide:
-    :ref:`howto/operator:TeradataToTeradataOperator`
+    :ref:`howto/operator:S3ToTeradataOperator`
     :param teradata_destination_conn_id: destination Teradata connection.
     :param destination_table: destination table to insert rows.
     :param teradata_source_conn_id: :ref:`Source Teradata connection <howto/connection:Teradata>`.
