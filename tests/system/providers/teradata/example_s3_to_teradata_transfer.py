d610e279e2fe732e09adc38c4f7c27253eca7fc0
Satish Ch <satishchinthanippu@gmail.com>
2024-06-20 13:00:02 +0530

    Added support of teradata authorization object for cloud transfer operators to teradata.
    1. Added teradata authorization object for authorization in transfer operators
    2. Added security token support in s3toteradata transfer operator

--- tests/system/providers/teradata/example_s3_to_teradata_transfer.py
+++ tests/system/providers/teradata/example_s3_to_teradata_transfer.py
@@ -54,7 +54,7 @@ with DAG(
     catchup=False,
     default_args={"teradata_conn_id": CONN_ID},
 ) as dag:
-    # [START s3_to_teradata_transfer_operator_howto_guide_transfer_data_s3_to_teradata_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_transfer_data_public_s3_to_teradata_csv]
     transfer_data_csv = S3ToTeradataOperator(
         task_id="transfer_data_s3_to_teradata_csv",
         s3_source_key="/s3/td-usgs-public.s3.amazonaws.com/CSVDATA/09394500/2018/06/",
@@ -63,7 +63,7 @@ with DAG(
         aws_conn_id="aws_default",
         trigger_rule="all_done",
     )
-    # [END s3_to_teradata_transfer_operator_howto_guide_transfer_data_s3_to_teradata_csv]
+    # [END s3_to_teradata_transfer_operator_howto_guide_transfer_data_public_s3_to_teradata_csv]
     # [START s3_to_teradata_transfer_operator_howto_guide_read_data_table_csv]
     read_data_table_csv = TeradataOperator(
         task_id="read_data_table_csv",
@@ -74,6 +74,54 @@ with DAG(
     # [START s3_to_teradata_transfer_operator_howto_guide_drop_table_csv]
     drop_table_csv = TeradataOperator(
         task_id="drop_table_csv",
+        conn_id=CONN_ID,
+        sql="DROP TABLE example_s3_teradata_csv;",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_drop_table_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_transfer_data_access_s3_to_teradata_csv]
+    transfer_key_data_csv = S3ToTeradataOperator(
+        task_id="transfer_key_data_s3_to_teradata_key_csv",
+        s3_source_key="/s3/airflowteradatatest.s3.ap-southeast-2.amazonaws.com/",
+        teradata_table="example_s3_teradata_csv",
+        aws_conn_id="aws_default",
+        teradata_conn_id="teradata_default",
+        trigger_rule="all_done",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_transfer_data_access_s3_to_teradata_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_read_data_table_csv]
+    read_key_data_table_csv = TeradataOperator(
+        task_id="read_key_data_table_csv",
+        conn_id=CONN_ID,
+        sql="SELECT * from example_s3_teradata_csv;",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_read_data_table_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_drop_table_csv]
+    drop_key_table_csv = TeradataOperator(
+        task_id="drop_key_table_csv",
+        conn_id=CONN_ID,
+        sql="DROP TABLE example_s3_teradata_csv;",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_drop_table_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_transfer_data_authorization_s3_to_teradata_csv]
+    transfer_auth_data_csv = S3ToTeradataOperator(
+        task_id="transfer_auth_data_s3_to_teradata_auth_csv",
+        s3_source_key="/s3/teradata-download.s3.us-east-1.amazonaws.com/DevTools/csv/",
+        teradata_table="example_s3_teradata_csv",
+        teradata_authorization_name="aws_authorization",
+        teradata_conn_id="teradata_default",
+        trigger_rule="all_done",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_transfer_data_authorization_s3_to_teradata_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_read_data_table_csv]
+    read_auth_data_table_csv = TeradataOperator(
+        task_id="read_auth_data_table_csv",
+        conn_id=CONN_ID,
+        sql="SELECT * from example_s3_teradata_csv;",
+    )
+    # [END s3_to_teradata_transfer_operator_howto_guide_read_data_table_csv]
+    # [START s3_to_teradata_transfer_operator_howto_guide_drop_table_csv]
+    drop_auth_table_csv = TeradataOperator(
+        task_id="drop_auth_table_csv",
         conn_id=CONN_ID,
         sql="DROP TABLE example_s3_teradata_csv;",
     )
@@ -116,12 +164,13 @@ with DAG(
         sql="SELECT * from example_s3_teradata_parquet;",
     )
     # [END s3_to_teradata_transfer_operator_howto_guide_read_data_table_parquet]
-    # [START s3_to_teradata_transfer_operator_howto_guide_drop_table_parquet]
+    # [START s3_to_teradata_transfer_operator_howto_guide_drop_table]
     drop_table_parquet = TeradataOperator(
         task_id="drop_table_parquet",
         sql="DROP TABLE example_s3_teradata_parquet;",
     )
-    # [END s3_to_teradata_transfer_operator_howto_guide_drop_table_parquet]
+
+    # [END s3_to_teradata_transfer_operator_howto_guide_drop_table]
     (
         transfer_data_csv
         >> transfer_data_json
@@ -132,6 +181,12 @@ with DAG(
         >> drop_table_csv
         >> drop_table_json
         >> drop_table_parquet
+        >> transfer_key_data_csv
+        >> read_key_data_table_csv
+        >> drop_key_table_csv
+        >> transfer_auth_data_csv
+        >> read_auth_data_table_csv
+        >> drop_auth_table_csv
     )
     # [END s3_to_teradata_transfer_operator_howto_guide]
 
